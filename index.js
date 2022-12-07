df049eadde25d3b7b238366b57e5ec71ce9e018c
meggidy <mimamitchelle@gmail.com>
2022-12-07 17:29:07 +0300

    Assignment progress

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+const form = document.getElementById("form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
+
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email,
+    }),
+  })
+    .then((resp) => resp.json)
+    .then((data) => console.log(data))
+    .catch(function (error) {
+      alert("Bad things! Ragnarők!");
+      console.log(error.message);
+    });
+}
+submitData();
