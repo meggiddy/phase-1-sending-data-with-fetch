025ffb7f92f7a9bf25350ca055cef1cef83d0178
meggidy <mimamitchelle@gmail.com>
2022-12-07 18:50:57 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 // Add your code here
 const form = document.getElementById("form");
-form.addEventListener("submit", (e) => {
-  //e.preventDefault();
-});
-
 function submitData(name, email) {
   return fetch("http://localhost:3000/users", {
     method: "POST",
@@ -16,11 +12,21 @@ function submitData(name, email) {
       email: email,
     }),
   })
-    .then((resp) => resp.json)
-    .then((data) => console.log(data))
+    .then((resp) => resp.json())
+    .then((data) => {
+      const id = document.createElement("span");
+      id.innerHTML = data.id;
+      form.append(id);
+    })
     .catch(function (error) {
-      alert("Bad things! Ragnarők!");
-      console.log(error.message);
+      const errorMessage = document.createElement("span");
+      errorMessage.innerHTML = error.message;
+      form.append(errorMessage);
     });
 }
-submitData();
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let element = e.target.elements;
+  submitData(element.userName.value, element.email.value);
+});
